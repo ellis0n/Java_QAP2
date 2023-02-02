8493e01d2eeccdab2f419f295d315d6c1669fce0
glen may <ellisonglenmay@gmail.com>
2023-02-02 17:32:08 -0330

    init

--- src/MyLine.java
+++ src/MyLine.java
@@ -1,2 +1,100 @@
-package PACKAGE_NAME;public class MyLine {
+public class MyLine {
+    private MyPoint begin;
+    private MyPoint end;
+
+    public MyLine(int x1, int y1, int x2, int y2) {
+        this.begin = new MyPoint(x1, y1);
+        this.end = new MyPoint(x2, y2);
+    }
+
+    public MyLine(MyPoint begin, MyPoint end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public MyPoint getBegin() {
+        return begin;
+    }
+
+    public MyPoint getEnd() {
+        return end;
+    }
+
+    public void setBegin(MyPoint begin) {
+        this.begin = begin;
+    }
+
+    public void setEnd(MyPoint end) {
+        this.end = end;
+    }
+
+    public int getBeginX() {
+        return begin.getX();
+    }
+
+    public int getBeginY() {
+        return begin.getY();
+    }
+
+    public int getEndX() {
+        return end.getX();
+    }
+
+    public int getEndY() {
+        return end.getY();
+    }
+
+    public void setBeginX(int x) {
+        begin.setX(x);
+    }
+
+    public void setBeginY(int y) {
+        begin.setY(y);
+    }
+
+    public void setBeginXY(int x, int y) {
+        begin.setXY(x, y);
+    }
+
+    public void setEndX(int x) {
+        end.setX(x);
+    }
+
+    public void setEndY(int y) {
+        end.setY(y);
+    }
+
+    public void setEndXY(int x, int y) {
+        end.setXY(x, y);
+    }
+
+    public int[] getBeginXY() {
+        return begin.getXY();
+    }
+
+    public int[] getEndXY() {
+        return end.getXY();
+    }
+
+    public void setBeginXY(int[] xy) {
+        begin.setXY(xy[0], xy[1]);
+    }
+
+    public void setEndXY(int[] xy) {
+        end.setXY(xy[0], xy[1]);
+    }
+
+    public String toString() {
+        return "MyLine[begin=" + begin + ",end=" + end + "]";
+    }
+
+    public double getLength() {
+        return begin.distance(end);
+    }
+
+
+
+    public double getGradient() {
+        return (end.getY() - begin.getY()) / (end.getX() - begin.getX());
+    }
 }
--- src/MyPoint.java
+++ src/MyPoint.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class MyPoint {
+public class MyPoint {
+    private int x = 0;
+    private int y = 0;
+
+    public MyPoint() {
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int[] getXY() {
+        int[] result = {x, y};
+        return result;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+    public double distance(int x, int y) {
+        int xDiff = this.x - x;
+        int yDiff = this.y - y;
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+
+    public double distance(MyPoint another) {
+        int xDiff = this.x - another.x;
+        int yDiff = this.y - another.y;
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+
+    public double distance() {
+        return Math.sqrt(x * x + y * y);
+    }
 }
